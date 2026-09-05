ba14340ec984f29043d02264f8167db3efbcb6a9
sankeerthana-cpu <sankeerthana2636@gmail.com>
2026-09-05 15:39:51 +0530

    Update heloo.java

--- heloo.java
+++ heloo.java
@@ -1,6 +1,6 @@
 public class heloo {
     public static void main(String[] args) {
-        System.out.println("Hello World godd evening");
+        System.out.println("Hello World good afternoon");
     }
 }
 
